07d71618f2fc9c2452e10311092ce45348889b75
wangqin0528 <302684034@qq.com>
2020-04-11 02:35:32 +0800

    master主支线

--- src/pages/Home/ListContainer/ListContainer.vue
+++ src/pages/Home/ListContainer/ListContainer.vue
@@ -81,7 +81,7 @@
                     </li>
                 </ul>
                 <div class="ads">
-                    <img src="/images/ad1.png" />
+                    <img src="./images/ad1.png" />
                 </div>
             </div>
         </div>
@@ -176,7 +176,7 @@
                         width: 25%;
 
                         .list-item {
-                            background-image: url(/images/icons.png);
+                            background-image: url(../images/icons.png);
                             width: 61px;
                             height: 40px;
                             display: block;
